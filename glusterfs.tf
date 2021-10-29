8283e1c41d38b6cc8e4e8b09714142a54c12ad0e
YOUR-NAME <YOUR-EMAIL-ADDRESS>
2021-10-29 08:54:18 -0400

    fix size of volume

--- glusterfs.tf
+++ glusterfs.tf
@@ -9,43 +9,61 @@
 #------------------------- D A T A B A S E ------------------------------
 
 resource "google_compute_instance" "db" {
- count = var.db_count
- machine_type = var.machine["db"]
- name = "db-${count.index+1}"
- depends_on = [google_compute_subnetwork.db]
- 
- boot_disk {
-    initialize_params {image = var.image}
- }
-allow_stopping_for_update = true # help debug
-# disk {
-#	auto_delete = true
-#	type = "pd-standard"
-#	disk_size_gb=50
-# }
+  count        = var.db_count
+  machine_type = var.machine["db"]
+  name         = "db-${count.index + 1}"
+  depends_on   = [google_compute_subnetwork.db]
+
+  boot_disk {
+    initialize_params { image = var.image }
+  }
+  allow_stopping_for_update = true # help debug
+  # disk {
+  #	auto_delete = true
+  #	type = "pd-standard"
+  #	disk_size_gb=50
+  # }
   metadata = {
-   ssh-keys = "db:${file("./gcp_main.pub")}"
- }
- tags = ["allow-icmp", "allow-ssh", "allow-all"]
- network_interface {
-   subnetwork = "db"
+    ssh-keys = "db:${file("./gcp_main.pub")}"
+  }
+  tags = ["allow-icmp", "allow-ssh", "allow-all"]
+  network_interface {
+    subnetwork = "db"
+  }
+  lifecycle {
+    ignore_changes = ["attached_disk"]
   }
 }
 
 ##--------------------------- Disk -------------------------------
-#resource "google_compute_attached_disk" "default" {
-#	disk = google_compute_disk.d50[0].id
-#	instance = google_compute_instance.db[0].id
-#}
-#resource "google_compute_disk" "d50"{
-#	name = "50G disk"
-#type = "pd-hdd"
-#}
+resource "google_compute_attached_disk" "attach_db1" {
+  disk     = google_compute_disk.d50_1.id
+  instance = google_compute_instance.db[0].id
+}
+resource "google_compute_attached_disk" "attach_db2" {
+  disk     = google_compute_disk.d50_2.id
+  instance = google_compute_instance.db[1].id
+}
+resource "google_compute_disk" "d50_2" {
+#  count = var.db_count
+  name  = "disk-50g-1"
+  type  = "pd-standard"
+  size  = 50
+}
+resource "google_compute_disk" "d50_1" {
+#  count = var.db_count
+  name  = "disk-50g-2"
+  type  = "pd-standard"
+  size  = 50
+}
+
+
+
 ##--------------------------- Disk -------------------------------
 
 output "DB-glusterfs-1" {
- value = google_compute_instance.db[0].network_interface[0].network_ip
+  value = google_compute_instance.db[0].network_interface[0].network_ip
 }
 output "DB-glusterfs-2" {
- value = google_compute_instance.db[1].network_interface[0].network_ip
+  value = google_compute_instance.db[1].network_interface[0].network_ip
 }
