94e2fcb44b18c09489c23623cb8e29fb1864c3d1
YOUR-NAME <YOUR-EMAIL-ADDRESS>
2021-11-05 08:24:08 -0400

    v1.0.0 with config migrations

--- vpc.tf
+++ vpc.tf
@@ -8,7 +8,6 @@ resource "google_compute_subnetwork" "private" {
   ip_cidr_range = "10.10.1.0/24"
   region        = var.region
   network       = google_compute_network.main_vpc.id
-  depends_on = [google_compute_network.main_vpc]
 }
 
 resource "google_compute_subnetwork" "public" {
@@ -16,7 +15,6 @@ resource "google_compute_subnetwork" "public" {
   ip_cidr_range = "10.10.2.0/24"
   region        = var.region
   network       = google_compute_network.main_vpc.id
-  depends_on = [google_compute_network.main_vpc]  # Dependence on the main network. It is created after main_vpc and is destroyed before it
 }
 
 resource "google_compute_subnetwork" "db" {
@@ -24,14 +22,12 @@ resource "google_compute_subnetwork" "db" {
   ip_cidr_range = "10.10.3.0/29"
   region        = var.region
   network       = google_compute_network.main_vpc.id
-  depends_on = [google_compute_network.main_vpc] # Dependence on the main network. It is created after main_vpc and is destroyed before it
 }
 #-------------------- G A T E W A Y --------------------------------
 resource "google_compute_router" "router" {
   name    = "my-router"
   region  = google_compute_subnetwork.private.region
   network = google_compute_network.main_vpc.id
-  depends_on = [google_compute_subnetwork.private] # Dependence on a private network. It is created after it and is destroyed before it
 
   bgp {
     asn = 64514
@@ -44,7 +40,6 @@ resource "google_compute_router_nat" "nat" {
   region                             = google_compute_router.router.region
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
-  depends_on = [google_compute_router.router] # Dependence on the router
   log_config {
     enable = true
     filter = "ERRORS_ONLY"
