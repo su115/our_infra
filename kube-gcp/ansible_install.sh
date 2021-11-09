d289eadaf26f877d246dc908429911558e5247a7
Qantass <82313520+Qantass@users.noreply.github.com>
2021-11-09 12:00:01 +0200

    add ansible

---
+++ kube-gcp/ansible_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir ansible
+cd ansible
+mkdir tmp
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y python3-pip
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 2
+sudo pip3 install ansible
